7c1802245fe69062ffe032d65aecffe1f7564859
Pablo <pablo.aguirredesouza@ebgames.com>
2022-04-21 08:11:05 +1000

    Basic List

--- WebApplication1/Pages/Index.cshtml.cs
+++ WebApplication1/Pages/Index.cshtml.cs
@@ -1,19 +1,38 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Data;
+using System.Globalization;
+using System.Linq;
+using Dapper;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Data.SqlClient;
+using WebApplication1.Models;
 
 namespace WebApplication1.Pages
 {
     public class IndexModel : PageModel
     {
-        private readonly ILogger<IndexModel> _logger;
+        private readonly IConfiguration _configuration;
 
-        public IndexModel(ILogger<IndexModel> logger)
+        public IEnumerable<Sleep> Records { get; set; }
+
+        public IndexModel(IConfiguration configuration)
         {
-            _logger = logger;
+            _configuration = configuration;
         }
 
         public void OnGet()
         {
+            Records = GetAllRecords();
+        }
+
+        private IEnumerable<Sleep> GetAllRecords()
+        {
+            using (IDbConnection connection = new SqlConnection(_configuration.GetConnectionString("ConnectionString")))
+            {
+                var allSleeps = connection.Query<Sleep>("SELECT * FROM Sleep");
+
+                return allSleeps;
+            }
 
         }
     }
